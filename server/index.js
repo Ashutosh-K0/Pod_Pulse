51cae9bf787a5cf6a62a4c680b694f5075926a52
Ashutosh Kumar <ashutosh.kumar.26.004@gmail.com>
2024-07-11 12:25:51 +0530

    Add files via upload

---
+++ server/index.js
@@ -0,0 +1,29 @@
+require("dotenv").config();
+require("express-async-errors");
+const express = require("express");
+const cors = require("cors");
+const connection = require("./db");
+const userRoutes = require("./routes/users");
+const authRoutes = require("./routes/auth");
+const songRoutes = require("./routes/songs");
+const playlistRoutes = require("./routes/playlists");
+const searchRoutes = require("./routes/search");
+const app = express();
+
+connection();
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/users", userRoutes);
+app.use("/api/login", authRoutes);
+app.use("/api/songs", songRoutes);
+app.use("/api/playlists", playlistRoutes);
+app.use("/api/", searchRoutes);
+
+// Define the port from environment variables or default to 8080
+const port = process.env.PORT || 8080;
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Listening on port ${port}...`);
+});
