ec7ab69b09041cf9971f662918864b3e13c93098
Ashutosh Kumar <ashutosh.kumar.26.004@gmail.com>
2024-07-11 12:34:38 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -61,7 +61,7 @@ const App = () => {
         <Route path="/login" component={Login} />
         <Route path="/not-found" component={NotFound} />
         <Redirect to="/not-found" />
-      </Switch>
+      </Routes>
     </Fragment>
   );
 };
